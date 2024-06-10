d4a43bcfbc68f16e88f969c389532ab55f299f76
MohammadRezaSaeeedi <M.R.Saeedi2024@gmail.com>
2024-06-11 00:19:12 +0330

    add component styled

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useProducts } from '../context/ProductsProvider'
-
+import styles from "./ProductsPage.module.css";
 function ProductsPage() {
   const products = useProducts();
   console.log(products)
   return (
-    <div>ProductsPage</div>
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p> }
+        {products.map((p) => (
+          <p key={p.id} >{p.title}</p>
+        ) )}
+      </div>
+      <div>sidebar</div>
+    </div>
   )
 }
 
