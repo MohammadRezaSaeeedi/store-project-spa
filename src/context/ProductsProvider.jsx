663cdbcd2c4ece21536ed623fcb83e1895ce1a6c
MohammadRezaSaeeedi <M.R.Saeedi2024@gmail.com>
2024-06-10 23:56:59 +0330

    add components  productsProvider

---
+++ src/context/ProductsProvider.jsx
@@ -0,0 +1,34 @@
+import React, { useContext ,  createContext , useEffect , useState } from 'react'
+import api from "../services/config"
+const ProductContext = createContext();
+
+function ProductsProvider({children}) {
+const [ products , setProducts] = useState([]);
+
+useEffect(() => {
+    const fetchProducts = async () => {
+        try {
+            // const response = await api.get("/products");
+            // setProducts(response);
+            setProducts(await api.get("/products"));
+            
+        } catch (error) {
+            console.log(error.message , "click")
+        }
+    };
+    fetchProducts();
+} ,[]);
+  return (
+  <ProductContext.Provider value={products}>
+    {children}
+  </ProductContext.Provider>
+  ) 
+}
+
+
+const useProducts = () => {
+  const products = useContext(ProductContext)
+  return products
+}
+export default ProductsProvider
+export { useProducts}
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useProducts } from '../context/ProductsProvider'
 
 function ProductsPage() {
+  const products = useProducts();
+  console.log(products)
   return (
     <div>ProductsPage</div>
   )
